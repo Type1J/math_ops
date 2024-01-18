48512ebd1bac64988270f21aa46cea1d89e612dc
Jay Sistar <jaysistar@yahoo.com>
2024-01-18 10:21:53 -0500

    More updates

--- math_ops_ui_run.sh
+++ math_ops_ui_run.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-docker run -it --rm -p 8042:80 math_ops_ui:latest
+ip_address=$(hostname -I | awk '{print $1}')
+docker run \
+  -it --rm \
+  -p 8042:80 \
+  -e REST_BASE_URL=http://$ip_address:8000 \
+  -e GRPC_WEB_URL=http://$ip_address:8051 \
+  math_ops_ui:latest
